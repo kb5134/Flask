8aca3e272a16191f0d18e180b29976c680b496bf
kb5134 <frrssatto@gmail.com>
2022-08-04 00:53:17 -0300

    Função LogOut Implementada e curso YT finalizado

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,9 +1,9 @@
 from curses import flash
-from flask import render_template, flash
+from flask import render_template, flash, url_for, redirect
 from app import app
 from app.models.form import LoginForm
 from app.models.tables import User
-from flask_login import login_user
+from flask_login import login_user, logout_user
 
 
 @app.route('/index/<user>')
@@ -19,9 +19,13 @@ def login():
         if user and user.password == form.password.data:
             login_user(user)
             flash("logado")
+            return redirect(url_for("index"))
         else:
             flash('Login Invalido')
-    else:
-        print (form.errors)
     return render_template('login.html', form=form)
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('deslogado')
+    return redirect(url_for('index'))
